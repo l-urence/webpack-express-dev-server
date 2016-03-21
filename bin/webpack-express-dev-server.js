d2a8dca4121bdce9aecc13f5ac2349391d1bb90f
Laurence Bortfeld <cheese@byteburgers.com>
2016-03-21 14:28:45 +0100

    Add cli help

--- bin/webpack-express-dev-server.js
+++ bin/webpack-express-dev-server.js
@@ -3,7 +3,12 @@
 'use strict';
 
 const Server = require('../src');
-const argv = require('yargs').argv;
+const argv = require('yargs')
+  .usage('Usage: webpack-express-dev-server [--watch]')
+  .help('h')
+  .alias('h', 'help')
+  .option('watch', { describe: 'Watch for file changes' })
+  .argv;
 
 const server = new Server({
   watch: Boolean(argv.watch)
